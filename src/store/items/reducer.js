b43cb9234ff11314cdb46272f02cee9720dfef35
Bharati Subramanian <bharatisharada@gmail.com>
2023-04-20 19:41:05 +0530

    feat: add item action, reducer case

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,12 +1,21 @@
+import { ITEM_ADDED } from "./actions";
+
 let id = 1;
 
 export const initialItems = [
-  { uuid: id++, name: 'Awesome Tofu Roast', price: 14, quantity: 1 },
-  { uuid: id++, name: 'Vegan Ham Sandwich', price: 12, quantity: 1 }
+    { uuid: id++, name: 'Awesome Tofu Roast', price: 14, quantity: 1 },
+    { uuid: id++, name: 'Vegan Ham Sandwich', price: 12, quantity: 1 }
 ];
 
 export const reducer = (state = initialItems, action) => {
-  return state;
+    if (action.type === ITEM_ADDED) {
+        const item = { uuid: id++, quantity: 1, ...action.payload };
+        return [
+            ...state,
+            item
+        ]
+    }
+    return state;
 };
 
 export default reducer;
