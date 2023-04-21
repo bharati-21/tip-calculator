e3ea3a03ce0ab1c8d523b0b1341a5cbf1f354416
Bharati Subramanian <bharatisharada@gmail.com>
2023-04-21 11:32:39 +0530

    feat: use immer to update state

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,3 +1,4 @@
+import produce from "immer";
 import { ITEM_ADDED, ITEM_PRICE_UPDATED, ITEM_QUANTITY_UPDATED, ITEM_REMOVED } from "./actions";
 
 let id = 1;
@@ -8,26 +9,40 @@ export const initialItems = [
 ];
 
 export const reducer = (state = initialItems, action) => {
-    if (action.type === ITEM_ADDED) {
+    if(action.type === ITEM_ADDED) {
         const item = { uuid: id++, quantity: 1, ...action.payload };
-        return [
-            ...state,
-            item
-        ]
+        //     return [
+        //         ...state,
+        //         item
+        //     ]
+
+        return produce(state, (draftState) => {
+            draftState.push(item)
+        })
     }
     if (action.type === ITEM_REMOVED) {
         const filteredItems = state.filter(item => item.uuid !== action.payload);
         return filteredItems;
     }
     if (action.type === ITEM_PRICE_UPDATED) {
-        return state.map(item => item.uuid === action.payload.uuid ? {
-            ...item, price: action.payload.price
-        } : item)
+        // return state.map(item => item.uuid === action.payload.uuid ? {
+        //     ...item, price: action.payload.price
+        // } : item)
+
+        return produce(state, (draftState) => {
+            const item = draftState.find(item => item.uuid === action.payload.uuid);
+            item.price = action.payload.price
+        })
     }
     if (action.type === ITEM_QUANTITY_UPDATED) {
-        return state.map(item => item.uuid === action.payload.uuid ? {
-            ...item, quantity: action.payload.quantity
-        } : item)
+        // return state.map(item => item.uuid === action.payload.uuid ? {
+        //     ...item, quantity: action.payload.quantity
+        // } : item)
+
+        return produce(state, (draftState) => {
+            const item = draftState.find(item => item.uuid === action.payload.uuid);
+            item.quantity = action.payload.quantity
+        })
     }
     return state;
 };
